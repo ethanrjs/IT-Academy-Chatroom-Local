00966c98f86ba0bf7852ea7525214210c415357c
ethanrettinger <ethan.rettinger@greatoaks.com>
2022-11-01 14:06:51 -0400

    4 2

--- public/login/js/formValidate.js
+++ public/login/js/formValidate.js
@@ -14,35 +14,6 @@ registerForm.addEventListener('submit', e => {
         return;
     }
 
-    // username must be 3-20 chars
-    if (username.length < 3 || username.length > 20) {
-        notify('Error', 'Username must be between 3 and 20 characters.');
-        return;
-    }
-
-    // displayname must be 3-40 chars
-    if (displayname.length < 3 || displayname.length > 40) {
-        notify('Error', 'Displayname must be between 3 and 40 characters.');
-        return;
-    }
-
-    // password must be 8-100 chars
-    if (password.length < 8 || password.length > 100) {
-        notify('Error', 'Password must be between 8 and 100 characters.');
-        return;
-    }
-
-    // username must be alphanumeric and can contain underscores and periods. it cannot start with a number or period
-    if (!/^[a-zA-Z0-9][a-zA-Z0-9_.]*$/.test(username)) {
-        notify('Error', 'Usernames are alphanumeric with _ and .');
-        return;
-    }
-
-    // displayname must be alphanumeric and can contain underscores and periods. it cannot start with a number or period
-    if (!/^[a-zA-Z0-9][a-zA-Z0-9_. ]*$/.test(displayname)) {
-        notify('Error', 'Displaynames are alphanumeric with _ and .');
-        return;
-    }
     register(username, displayname, password);
 });
 
